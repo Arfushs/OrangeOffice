1daf6c28204639bfd63b5e7606b0e3445bdb8b40
Mage <Tailgunnner@hotmail.com>
2022-11-19 00:21:56 +0300

    Movement_anim

--- TDS/Scripts/Player.gd
+++ TDS/Scripts/Player.gd
@@ -4,7 +4,8 @@ extends CharacterBody3D
 @export var SPEED = 10.0
 @export var Bullet: PackedScene
 @export var max_ammo = 10
-
+@onready var rotator:Node3D = $Rotation
+var input_dir: Vector2
 @onready var anim : AnimationTree = $Rotation/main_character/AnimationTree
 
 var ammo = max_ammo
@@ -14,6 +15,7 @@ signal ammo_spent(ammo,max_ammo)
 
 func _physics_process(delta):
 	movement_function()
+	animHandle()
 	shoot()
 	
 	if Input.is_action_just_pressed("reload") && ammo != max_ammo:
@@ -28,7 +30,7 @@ func movement_function():
 		
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var direction = (Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
@@ -67,10 +69,13 @@ func reload():
 	ammo = max_ammo
 	emit_signal("ammo_spent",ammo,max_ammo)
 		
+func animHandle():
+	
+	var tempVector1 = rotator.global_transform.basis * Vector3(input_dir.x,0,input_dir.y)
+	print(tempVector1.normalized())
+	var animVector:= Vector2(tempVector1.x,tempVector1.z)
+	anim.set("parameters/Movement_vector/blend_position", animVector)
+	if (velocity != Vector3(0,0,0)):
+		anim.set("parameters/turnblending/blend_amount", 1)
 		
-		
-		
-		
-		
-		
-		
+
